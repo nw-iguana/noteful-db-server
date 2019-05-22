2451e3f6b7f8241d9808ce789e4d9946bf4ad37f
Ryan Cahela <ryancahela@gmail.com>
2019-05-21 20:58:05 -0700

    db setup and seed files writen, ready for routing

---
+++ seeds/seed.noteful_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO folders (folder_title)
+    VALUES
+    ('Ryan-is-cooler-than'),
+    ('Feeeeeeeelix'),
+    ('Anya');
+
+INSERT INTO notes (note_name, note_content, folder_id)
+VALUES
+    ('homework', 'math and science', 1),
+    ('groceries', 'meat and cheese', 2),
+    ('watching tv', 'The View', 3);
